120b05d1925be72cd2f7f6b417e0e17ca39af03a
Patrick Chiu <42149082+patrick-kw-chiu@users.noreply.github.com>
2024-05-29 00:14:39 +0800

    Update docs

---
+++ docs/components/Accordion.tsx
@@ -0,0 +1,25 @@
+import { useState } from 'react';
+
+const Accordion = ({ label = '', children }) => {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <div
+      style={{
+        border: open ? '1px solid rgba(151, 151, 151, 0.5)' : '',
+        padding: '0.5rem',
+      }}
+    >
+      <div
+        className="nx-font-semibold nx-tracking-tight nx-text-slate-900 dark:nx-text-slate-100 nx-text-xl"
+        style={{ cursor: 'pointer', paddingBottom: '0.5rem' }}
+        onClick={() => setOpen(!open)}
+      >
+        ► {label}
+      </div>
+      {open && <div style={{ padding: '0.5rem 0' }}>{children}</div>}
+    </div>
+  );
+};
+
+export default Accordion;
